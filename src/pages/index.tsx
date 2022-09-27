07df0a2878a0bc2699278a9fbe3222f42937a95d
ggfevans <63365672+ggfevans@users.noreply.github.com>
2022-09-27 01:02:35 -0700

    fix eslint problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
-
 import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
