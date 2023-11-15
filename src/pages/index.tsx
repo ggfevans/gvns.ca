e61435d716792b5957c5d7a7568264be12c3e142
Gareth Evans <hi@gvns.ca>
2023-11-15 00:18:39 -0800

    adding firebase to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import {getAnalytics} from "firebase/analytics";
+import {initializeApp} from "firebase/app";
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
@@ -10,6 +12,20 @@ import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-nS9FEMjth4ryab01z-Qndb94br7-8qQ",
+  authDomain: "gvns-ca.firebaseapp.com",
+  projectId: "gvns-ca",
+  storageBucket: "gvns-ca.appspot.com",
+  messagingSenderId: "376773105450",
+  appId: "1:376773105450:web:ac0e7afad342f68d1ff053",
+  measurementId: "G-8VZMZG93BY"
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export const analytics = typeof window !== "undefined" ? getAnalytics(firebase) : null;
 
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
